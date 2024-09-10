59574e8380c3ae9fbaed82a83799f2bbbea884bd
ISMAIL2099 <lil_isma@icloud.com>
2024-09-10 11:51:33 +0200

    commit

--- 16-Listes_mutables/16_4-Boucles_for_sur_listes/P16_4_c-Triples/src/main/kotlin/main.kt
+++ 16-Listes_mutables/16_4-Boucles_for_sur_listes/P16_4_c-Triples/src/main/kotlin/main.kt
@@ -1,7 +1,7 @@
 fun main() {
-    var liste = listOf(1,2,3,5,4,6,8,4,5,6,7,8,5,4,3,4,5)
+    var liste = listOf(7,2,3,5,4,6,8,4,5,6,7,8,5,4,1,2,3)
     var triples = 0
-    for (i in liste) {
+    for (i in 0..liste.size-3) {
         if (liste[i] + 1 == liste[i+1] && liste[i+2] == liste[i] + 2){
             triples += 1
         }
